0e95d697123ab4576047fe644d55b4fae334031f
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-06-23 16:35:37 -0700

    Commit for the methods submission assignemnt

---
+++ Submit assignments/Methods-Submission-Assignment/Methods-Submission-Assignment/Maths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Submission_Assignment
+{
+    // Declares a new class
+    class Maths
+    {
+        // Creates a method that returns a given number added to itself
+        public static int Add(int number)
+        {
+            return number + number;
+        }
+
+        // Creates a method that rutsn a given number multiplied by itself
+        public static int Multiply(int number)
+        {
+            return number * number;
+        }
+
+        // Creates a method that returns a given muber divided by itself
+        public static int Divide(int number)
+        {
+            return number / number;
+        }
+    }
+}
---
+++ Submit assignments/Methods-Submission-Assignment/Methods-Submission-Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Submission_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // provides instructions to the user then requests an string input which is converted into an integer.
+            Console.WriteLine("Please Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Calls the Maths.Add method
+            Console.WriteLine(Maths.Add(number));
+
+            // Calls the Maths.Multiply method
+            Console.WriteLine(Maths.Multiply(number));
+
+            // Calls the Maths.Divide method
+            Console.WriteLine(Maths.Divide(number));
+
+            Console.Read();
+        }
+    }
+}
