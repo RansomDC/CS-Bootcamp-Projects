e3c317d2307a440dfb50ef61ec516fe4b4aba987
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-06-24 10:11:05 -0700

    Commit for Method submission assignment

---
+++ Submit assignments/Method-Submission-Assignment/Method-Submission-Assignment/Maths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_Assignment
+{
+    class Maths
+    {
+        public int MathIsFun(int num1, int num2 = 1)
+        {
+            return (num1 * 2) * num2;
+        }
+    }
+}
---
+++ Submit assignments/Method-Submission-Assignment/Method-Submission-Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Maths mathTest = new Maths();
+
+            Console.WriteLine("Please enter a whole number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number (Optional, press 'Enter' to skip)");
+            string input2 = Console.ReadLine();
+           
+
+            if (input2 == "")
+            {
+                Console.WriteLine(mathTest.MathIsFun(num1));
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(input2);
+                Console.WriteLine(mathTest.MathIsFun(num1, num2));
+            }
+
+            Console.Read();
+        }
+    }
+}
