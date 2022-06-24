9376fd6c63304ca218df28305c6d0b9a5378e55f
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-06-24 10:24:13 -0700

    update comments on Method assignment

--- Submit assignments/Method-Submission-Assignment/Method-Submission-Assignment/Maths.cs
+++ Submit assignments/Method-Submission-Assignment/Method-Submission-Assignment/Maths.cs
@@ -8,6 +8,7 @@ namespace Method_Submission_Assignment
 {
     class Maths
     {
+        // Method with second parameter being optional (default is 1)
         public int MathIsFun(int num1, int num2 = 1)
         {
             return (num1 * 2) * num2;
--- Submit assignments/Method-Submission-Assignment/Method-Submission-Assignment/Program.cs
+++ Submit assignments/Method-Submission-Assignment/Method-Submission-Assignment/Program.cs
@@ -12,20 +12,26 @@ namespace Method_Submission_Assignment
         {
             Maths mathTest = new Maths();
 
+            // print instruction for user and get input. Convert input to integer
             Console.WriteLine("Please enter a whole number");
             int num1 = Convert.ToInt32(Console.ReadLine());
 
+            // print instruction for user and get input as string
             Console.WriteLine("Enter a second number (Optional, press 'Enter' to skip)");
             string input2 = Console.ReadLine();
            
-
+            // if user chose to not enter second input run method with default second parameter
             if (input2 == "")
             {
+                // Call and print class method
                 Console.WriteLine(mathTest.MathIsFun(num1));
             }
+            // if user chose to enter value for second input run method with manual second parameter
             else
             {
+                // Convert second input to integer
                 int num2 = Convert.ToInt32(input2);
+                // Call and print class method
                 Console.WriteLine(mathTest.MathIsFun(num1, num2));
             }
 
