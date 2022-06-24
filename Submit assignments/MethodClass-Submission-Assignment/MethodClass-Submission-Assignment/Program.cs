b8daab85bc222e71462be8f44356322994431936
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-06-24 10:35:27 -0700

    commit for Method class submission assignment

--- Submit assignments/MethodClass-Submission-Assignment/MethodClass-Submission-Assignment/Program.cs
+++ Submit assignments/MethodClass-Submission-Assignment/MethodClass-Submission-Assignment/Program.cs
@@ -8,8 +8,17 @@ namespace MethodClass_Submission_Assignment
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            myClass letsGo = new myClass();
+
+            // Calling method with the default pattern
+            letsGo.funk(2, 3);
+
+            // Calling method declaring parameters by name
+            letsGo.funk(operate: 4, display: 5);
+
+            Console.Read();
         }
     }
 }
--- Submit assignments/MethodClass-Submission-Assignment/MethodClass-Submission-Assignment/myClass.cs
+++ Submit assignments/MethodClass-Submission-Assignment/MethodClass-Submission-Assignment/myClass.cs
@@ -8,5 +8,13 @@ namespace MethodClass_Submission_Assignment
 {
     class myClass
     {
+        // Void method does not return any data 
+        public void funk(int operate, int display)
+        {
+            // perform math operation but doesn't return
+            int num3 = operate * 2;
+
+            Console.WriteLine(display);
+        }
     }
 }
